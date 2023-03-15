8ac282c71dff5ab294360ce94da83db755b64fff
TPL1122_18 <tpl1122_18@TPL1122-18s-MacBook-Pro.local>
2023-03-15 08:21:21 -0700

    added post request

--- server/server.js
+++ server/server.js
@@ -33,21 +33,29 @@ app.get('/api/events', async (req, res) =>{
         console.log(error);
         return res.status(400).json({error});
 
-    }
+    }})
 
-    //hardcode the events response for testing reasons. This call has one more event that the real DB 
-    // const events = [
 
-    //     {id: 1, title: 'Women in Tech Techtonica Panel', location: 'Overland Park Convention Center'},
-    //     {id: 2, title: 'Japanese Cultural Education', location: 'Seattle Convention Center'},
-    //     {id: 3, title: "Haven 90's Party Night Club", location: 'Hilton Hotel Kansas City'},
-    //     {id: 4, title: 'Comedy Night at the Station', location: 'SF Hilton Hotel'},
-    //     {id: 5, title: 'A Decadent Arts Experience', location: 'West Ridge Mall'},
-    //     {id: 6, title: 'Techtonica Classroom Course', location: 'Techtonica HQ'}
-    //   ];
-    // res.json(events);
-})
-
+    app.post("/api/events", async (req, res) => {
+        //TO - DO - At the end => save this event to the db
+        //INSERT INTO events (title, location, eventtime) VALUES ('Women in Tech Techtonica Panel', 'Overland Park Convention Center', '2023-04-21')
+        try {
+            const newEvent = {
+                title: req.body.title,
+                location: req.body.location,
+                eventtime: req.body.eventtime
+            }
+            const result = await db.query('INSERT INTO events(title, location, eventtime) VALUES ($1, $2, $3) RETURNING *', [newEvent.title, newEvent.location, newEvent.eventtime]);
+            let response = result.rows[0];
+            console.log(response);
+            res.json(response)
+    
+        } catch (e){
+            console.log(error);
+            return res.status(400).json({error});
+        }
+    })
+    
 
 
 app.listen(PORT, () => console.log(`Hola! Server running on Port http://localhost:${PORT}`));
